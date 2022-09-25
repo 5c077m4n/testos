671774e4c2a4800ccb76887211345a435451053a
Roee Shapira <roee.shapira@tensorleap.ai>
2022-09-25 11:28:45 +0300

    Add PIC

--- src/lib/interrupts.rs
+++ src/lib/interrupts.rs
@@ -1,12 +1,34 @@
-use super::println;
-use crate::gdt;
+use super::{gdt, print, println};
 use lazy_static::lazy_static;
+use pic8259::ChainedPics;
+use spin::Mutex;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+
+pub const PIC_1_OFFSET: u8 = 32;
+pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
+
+pub static PICS: Mutex<ChainedPics> =
+	Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
+
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum InterruptIndex {
+	Timer = PIC_1_OFFSET,
+}
+impl InterruptIndex {
+	fn as_u8(self) -> u8 {
+		self as u8
+	}
+	fn as_usize(self) -> usize {
+		self.as_u8() as usize
+	}
+}
 
 lazy_static! {
 	static ref IDT: InterruptDescriptorTable = {
 		let mut idt = InterruptDescriptorTable::new();
 		idt.breakpoint.set_handler_fn(breakpoint_handler);
+		idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
 		unsafe {
 			idt.double_fault
 				.set_handler_fn(double_fault_handler)
@@ -24,6 +46,13 @@ extern "x86-interrupt" fn double_fault_handler(
 	_error_code: u64,
 ) -> ! {
 	panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+}
+extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
+	print!(".");
+	unsafe {
+		PICS.lock()
+			.notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
+	};
 }
 
 pub fn init_idt() {
