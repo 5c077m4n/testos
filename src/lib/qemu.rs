ba9dd69e0bd6d2ed1f1eaeed28f1b6bfddc6d79a
Roee Shapira <roee.shapira@tensorleap.ai>
2022-09-25 15:22:01 +0300

    Made port static

--- src/lib/qemu.rs
+++ src/lib/qemu.rs
@@ -1,24 +1,22 @@
-#![allow(dead_code)]
+use x86_64::instructions::port::Port;
 
-use core::{
-	cmp::{Eq, PartialEq},
-	prelude::rust_2021::*,
-};
+static mut SERIAL_PORT: Port<u32> = Port::new(0xf4);
 
-const SERIAL_PORT: u16 = 0xf4;
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
 pub enum ExitCode {
-	Success = 0x10,
-	Fail = 0x11,
+	Success,
+	Fail,
+}
+impl ExitCode {
+	pub fn as_u32(self) -> u32 {
+		match self {
+			Self::Success => 0x10,
+			Self::Fail => 0x11,
+		}
+	}
 }
 
 pub fn exit(exit_code: ExitCode) {
-	use x86_64::instructions::port::Port;
-
 	unsafe {
-		let mut port = Port::new(SERIAL_PORT);
-		port.write(exit_code as u32);
+		SERIAL_PORT.write(exit_code.as_u32());
 	}
 }
